94df9d8834da7d7280dadd190105274e7a701f37
Majaj13 <maelmerrer1309@gmail.com>
2023-03-17 14:07:31 +0100

    test2

---
+++ front_end/acceuil.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>APS2</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="../assets/css/main.css" />
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="author" content="Maël Merrer">
+        <meta name="description" content="Page d'acceuil psychoquiz">
+        <meta name="keywords" content="Acceuil, site, psychoquiz, le goat plassart">
+    </head>
+    <body>
+        <div class = "section1" ></div>
+        <h1> Psychoquiz </h1>
+        <h2> Plutot SLAM ou SISR </h2>
+        <h4> Êtes vous prêt à savoir quel option vous convient le mieux ? </h4>
+        <a for="switch" class="demarrer" href="">Démarrer</a>
+
+
+
+
+        <footer class="text-center text-lg-start bg-white text-muted">
+            <div class = "section2">
+                <div class="p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+                    © 2023 Copyright: TreizeOrganisé
+                </div>
+            </div>
+        </footer>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+</html>
