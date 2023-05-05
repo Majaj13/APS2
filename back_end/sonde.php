783719d15106478dc933491dda1a86000376f9a3
Kevin <ksider.ledantec@gmail.com>
2023-05-05 16:12:49 +0200

    Commit 05/05 KS

--- back_end/sonde.php
+++ back_end/sonde.php
@@ -1,32 +1,43 @@
 <?php
 
-// Connexion à la base de données quizz en administrateur ROOT
-
-require('../back_end/include/_inc_parametres.php');
+require('../back_end/include/_inc_parametres.php');   //Connexion base de données
 require('../back_end/include/_inc_connexion.php');
 
-// Vérification que les données ont été envoyées via la méthode POST
+try 
+{
+    $cnx->beginTransaction();
 
-if (isset($_POST['sexe']) && isset($_POST['IDORIGINE']))
+    // Vérification que les données ont été envoyées via la méthode POST
+    if (isset($_POST['sexe']) && isset($_POST['IDORIGINE'])) 
+    {
+        // Récupération des données (sexe et origine), ajout automatique de l'année
+        $sexe = $_POST['sexe'];
+        $IDORIGINE = $_POST['IDORIGINE'];
+        $annee = date("Y");
 
-    // Récupération des données (sexe et origine), ajout automatique de l'année
+        // Préparation de la requête d'insertion des données dans la table SQL "sonde"
+        $stmt = $cnx->prepare("INSERT INTO sonde (IDORIGINE, SEXE, ANNEE) VALUES (:IDORIGINE, :sexe, :annee)");
 
-    $sexe = $_POST['sexe'];
-    $IDORIGINE = $_POST['IDORIGINE'];
-    $annee = date("Y");
 
-    // Préparation de la requête d'insertion des données dans la table SQL "sonde"
+        // Exécution de la requête avec les valeurs des paramètres correspondants
+        $stmt->execute(array(':IDORIGINE' => $IDORIGINE, ':sexe' => $sexe, ':annee' => $annee));
+    } 
 
-    $stmt = $cnx->prepare("INSERT INTO sonde (IDORIGINE, SEXE, ANNEE) VALUES (:IDORIGINE, :sexe, :annee)");
-
-    // Exécution de la requête avec les valeurs des paramètres correspondants
-
-    $stmt->execute(array(':IDORIGINE' => $IDORIGINE, ':sexe' => $sexe, ':annee' => $annee));
+    // Validation de la transaction
+    $cnx->commit();
 
     // Redirection vers mon questionnaire
-    
-header("Location: ../front_end/quiz.php");
-exit();
+    header("Location: ../front_end/quiz.php");
+    exit();
+} 
+catch (Exception $e) //en cas d'erreur
+{
+    // Annulation de la transaction
+    $cnx->rollback();
+    echo "<br>Tout ne s'est pas bien passé, voir les erreurs ci-dessous :</br>";
+    echo "Erreur : " . $e->getMessage(). " N° : ".$e->getCode();   //Arrêt de l'éxécution si il y a du code après
+    exit();
+}
 
 // Fermeture de la connexion PDO
 $cnx = null;
